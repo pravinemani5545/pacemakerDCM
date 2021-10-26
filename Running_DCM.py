563a93f184fad08a465f3c2ae3830219a0bd439f
Pravine Manivannan <pravinemanivannan8@gmail.com>
2021-10-26 00:45:02 -0400

    Just added units

--- Running_DCM.py
+++ Running_DCM.py
@@ -245,6 +245,8 @@ def about():
 
 
 
+
+
 # AOO MODE PARAMETERS
 def set_mode_AOO():
     # set up AOO frame inside of pm_params frame
@@ -265,16 +267,16 @@ def set_mode_AOO():
     aa_value = StringVar()
     apw_value = StringVar()
 
-    Label(AOO_mode, text="Lower Rate Limit: ").grid(row=1, column=0, padx= 85, pady=2)
+    Label(AOO_mode, text="Lower Rate Limit (ppm): ").grid(row=1, column=0, padx= 85, pady=2)
     enter_lrl = Entry(AOO_mode, textvariable = lrl_value).grid(row=1, column=1)
 
-    Label(AOO_mode, text="Upper Rate Limit: ").grid(row=2, column=0, pady=2)
+    Label(AOO_mode, text="Upper Rate Limit (ppm): ").grid(row=2, column=0, pady=2)
     enter_url = Entry(AOO_mode, textvariable =url_value).grid(row=2, column=1)
 
-    Label(AOO_mode, text="Atrial Amplitude: ").grid(row=3, column=0, pady=2)
+    Label(AOO_mode, text="Atrial Amplitude (V): ").grid(row=3, column=0, pady=2)
     enter_aa = Entry(AOO_mode, textvariable=aa_value).grid(row=3, column=1)
 
-    Label(AOO_mode, text="Atrial Pulse Width: ").grid(row=4, column=0, pady=2)
+    Label(AOO_mode, text="Atrial Pulse Width (ms): ").grid(row=4, column=0, pady=2)
     enter_apw = Entry(AOO_mode, textvariable=apw_value).grid(row=4, column=1)
 
     Button(AOO_mode, text="Update", padx=20, pady=10, command=send_AOO).grid(row=5, columnspan=2)
@@ -364,21 +366,19 @@ def set_mode_VOO():
     va_value = StringVar()
     vpw_value = StringVar()
 
-    Label(VOO_mode, text="Lower Rate Limit: ").grid(row=1, column=0, padx= 85, pady=2)
+    Label(VOO_mode, text="Lower Rate Limit (ppm): ").grid(row=1, column=0, padx= 85, pady=2)
     enter_lrl = Entry(VOO_mode, textvariable=lrl_value).grid(row=1, column=1)
 
-    Label(VOO_mode, text="Upper Rate Limit: ").grid(row=2, column=0, pady=2)
+    Label(VOO_mode, text="Upper Rate Limit (ppm): ").grid(row=2, column=0, pady=2)
     enter_url = Entry(VOO_mode, textvariable=url_value).grid(row=2, column=1)
 
-    Label(VOO_mode, text="Ventrical Amplitude: ").grid(row=3, column=0, pady=2)
+    Label(VOO_mode, text="Ventrical Amplitude (V): ").grid(row=3, column=0, pady=2)
     enter_va = Entry(VOO_mode, textvariable=va_value).grid(row=3, column=1)
 
-    Label(VOO_mode, text="Ventrical Pulse Width: ").grid(row=4, column=0, pady=2)
+    Label(VOO_mode, text="Ventrical Pulse Width (ms): ").grid(row=4, column=0, pady=2)
     enter_vpw = Entry(VOO_mode, textvariable=vpw_value).grid(row=4, column=1)
 
     Button(VOO_mode, text="Update", padx=20, pady=10, command=send_VOO).grid(row=5, columnspan=2, pady=2)
-
-
 
 def send_VOO():
     mode = VOO()
@@ -466,19 +466,19 @@ def set_mode_AAI():
     apw_value = StringVar()
     arp_value = StringVar()
 
-    Label(AAI_mode, text="Lower Rate Limit: ").grid(row=1, column=0, padx=85 , pady=2)
+    Label(AAI_mode, text="Lower Rate Limit (ppm): ").grid(row=1, column=0, padx=85 , pady=2)
     enter_lrl = Entry(AAI_mode, textvariable=lrl_value).grid(row=1, column=1)
 
-    Label(AAI_mode, text="Upper Rate Limit: ").grid(row=2, column=0, pady=2)
+    Label(AAI_mode, text="Upper Rate Limit (ppm): ").grid(row=2, column=0, pady=2)
     enter_url = Entry(AAI_mode, textvariable=url_value).grid(row=2, column=1)
 
-    Label(AAI_mode, text="Atrial Amplitude: ").grid(row=3, column=0, pady=2)
+    Label(AAI_mode, text="Atrial Amplitude (V): ").grid(row=3, column=0, pady=2)
     enter_aa = Entry(AAI_mode, textvariable=aa_value).grid(row=3, column=1)
 
-    Label(AAI_mode, text="Atrial Pulse Width: ").grid(row=4, column=0, pady=2)
+    Label(AAI_mode, text="Atrial Pulse Width (ms): ").grid(row=4, column=0, pady=2)
     enter_apw = Entry(AAI_mode, textvariable=apw_value).grid(row=4, column=1)
 
-    Label(AAI_mode, text="ARP: ").grid(row=5, column=0, pady=2)
+    Label(AAI_mode, text="ARP (ms): ").grid(row=5, column=0, pady=2)
     enter_arp = Entry(AAI_mode, textvariable=arp_value).grid(row=5, column=1)
 
     Button(AAI_mode, text="Update", padx=20, pady=10, command=send_AAI).grid(row=6, columnspan=2 , pady=2)
@@ -579,19 +579,19 @@ def set_mode_VVI():
     vrp_value = StringVar()
 
 
-    Label(VVI_mode, text="Lower Rate Limit: ").grid(row=1, column=0, padx=85, pady = 2)
+    Label(VVI_mode, text="Lower Rate Limit (ppm): ").grid(row=1, column=0, padx=85, pady = 2)
     enter_lrl = Entry(VVI_mode, textvariable=lrl_value).grid(row=1, column=1)
 
-    Label(VVI_mode, text="Upper Rate Limit: ").grid(row=2, column=0, pady=2)
+    Label(VVI_mode, text="Upper Rate Limit (ppm): ").grid(row=2, column=0, pady=2)
     enter_url = Entry(VVI_mode, textvariable=url_value).grid(row=2, column=1)
 
-    Label(VVI_mode, text="Ventricular Amplitude: ").grid(row=3, column=0, pady=2)
+    Label(VVI_mode, text="Ventricular Amplitude (V): ").grid(row=3, column=0, pady=2)
     enter_va = Entry(VVI_mode, textvariable=va_value).grid(row=3, column=1)
 
-    Label(VVI_mode, text="Ventricular Pulse Width: ").grid(row=4, column=0, pady=2)
+    Label(VVI_mode, text="Ventricular Pulse Width (ms): ").grid(row=4, column=0, pady=2)
     enter_vpw = Entry(VVI_mode, textvariable=vpw_value).grid(row=4, column=1)
 
-    Label(VVI_mode, text="VRP: ").grid(row=5, column=0, pady=2)
+    Label(VVI_mode, text="VRP (ms): ").grid(row=5, column=0, pady=2)
     enter_vrp = Entry(VVI_mode, textvariable=vrp_value).grid(row=5, column=1)
 
     Button(VVI_mode, text="Update", padx=20, pady=10, command=send_VVI).grid(row=6, columnspan=2, pady= 2)
@@ -670,6 +670,8 @@ def send_VVI():
     print(mode.get_VPW())
     print(mode.get_VRP())
 
+
+
 # PACEMAKER PARAMETERS WINDOW
 def pacemaker_parameters():
     global pm_params
@@ -685,8 +687,6 @@ def pacemaker_parameters():
     mode_frame = Frame(pm_params)
     mode_frame.grid(row=1, column=0, columnspan=2)
 
-
-    #DO WE NEED TO PRESET ALL OF THEM ???????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
     set_mode_AOO()
     set_mode_VOO()
     set_mode_AAI()
